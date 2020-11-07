e1ef92850c8ef2f0c37b5dd25154437a2ac8d26c
Jeisson Gomez <jeypitbullz4@gmai.com>
2020-11-06 20:30:14 -0500

    delete_task.php

---
+++ delete_task.php
@@ -0,0 +1,17 @@
+<?php
+    include ("db.php");
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $query = "DELETE FROM control WHERE ID = $id";
+        $result = mysqli_query($conn, $query);
+        if(!$result){
+            die("Query Failed");
+        }
+
+        $_SESSION['message'] = 'Eliminado Exitosamente';
+        $_SESSION['message_type'] = 'danger';
+
+        header("location: index.php");
+    }
+?>
